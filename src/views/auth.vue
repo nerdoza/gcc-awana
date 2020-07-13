e13d6b6d99b667045836e172dde7c68046c12ad7
Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2020-07-12 18:20:06 -0700

    Updates auth wording

--- src/views/auth.vue
+++ src/views/auth.vue
@@ -5,7 +5,7 @@
         <v-col cols="12" sm="8" md="4">
           <v-card class="elevation-12">
             <v-toolbar color="primary" dark flat>
-              <v-toolbar-title>Welcome</v-toolbar-title>
+              <v-toolbar-title>GCC AWANA</v-toolbar-title>
               <v-spacer></v-spacer>
               <v-tooltip bottom>
                 <template v-slot:activator="{ on }">
@@ -17,7 +17,8 @@
               </v-tooltip>
             </v-toolbar>
             <v-card-text>
-              <div class="text--primary">Enter your phone number to get started.</div>
+              <div class="text--primary">Welcome!</div>
+              <div class="text--primary">Entering your mobile phone number below to get started.</div>
               <v-form>
                 <v-text-field
                   label="Phone"
@@ -27,10 +28,13 @@
                   v-mask="'+1 (###) ###-####'"
                 ></v-text-field>
               </v-form>
+              <div
+                class="text--primary"
+              >You will receive a text message with a code to complete your sign in.</div>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="primary">Login</v-btn>
+              <v-btn color="primary" :disabled="!phoneNumberValid">Send code</v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -49,6 +53,6 @@ import { mask } from 'vue-the-mask'
   }
 })
 export default class extends Vue {
-
+  phoneNumberValid = false
 }
 </script>
