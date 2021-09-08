77e8b16f99a41bb15ab979c33ce47d6322954fc9
Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2021-09-07 22:52:14 -0700

    Pushes to v2.0.1 Updates Firebase package

--- vue.config.js
+++ vue.config.js
@@ -29,8 +29,8 @@ module.exports = {
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
-      appleTouchIcon: 'img/icons/apple-icon-152x152.png',
-      msTileImage: 'img/icons/ms-icon-144x144.png'
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      msTileImage: 'img/icons/mstile-150x150.png'
     },
     manifestOptions: {
       icons: [
